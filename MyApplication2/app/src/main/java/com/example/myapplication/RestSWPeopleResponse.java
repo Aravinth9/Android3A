fc4af8a3c21e62dce0e58c8ccae50a034102a15d
Aravinth <aravinth.shanthir@gmail.com>
2020-05-21 18:41:43 +0200

    Ajout API Star Wars

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/RestSWPeopleResponse.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+public class RestSWPeopleResponse {
+
+    private String name;
+    private int height;
+    private int mass;
+    private String birth_year;
+    private String gender;
+    private String films;
+
+    public String getName() {
+        return name;
+    }
+
+    int getHeight() {
+        return height;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    String getBirth_year() {
+        return birth_year;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getFilms() {
+        return films;
+    }
+
+
+
+
+
+}
